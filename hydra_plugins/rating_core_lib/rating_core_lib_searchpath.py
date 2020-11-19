29e90ada23294be74de873c83210dc7f0c9ddf21
shay-te <shay.te@gmail.com>
2020-11-19 20:46:59 +0200

    fix plugin move to postgress

--- hydra_plugins/rating_core_lib/rating_core_lib_searchpath.py
+++ hydra_plugins/rating_core_lib/rating_core_lib_searchpath.py
@@ -5,4 +5,4 @@ from hydra.core.config_search_path import ConfigSearchPath
 class RatingCoreLibSearchPathPlugin(SearchPathPlugin):
     def manipulate_search_path(self, search_path: ConfigSearchPath) -> None:
         assert isinstance(search_path, ConfigSearchPath)
-        search_path.append("rating_core_lib", "pkg://rating_core_lib/config")
+        search_path.append("rating_core_lib", "pkg://rating_core_lib.config")
